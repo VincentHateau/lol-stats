42d1baff711513879120bd124f61d79d9c51db85
vha <vincent.hateau-kan@lesfurets.com>
2021-02-15 15:10:38 +0100

    Chargement du projet depuis IntelliJ

--- packages/back/src/app.ts
+++ packages/back/src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.get("/", (req, res) => {
+  res.send("The sedulous hyena ate the antelope!");
 });
 
 app.listen(port, () => {
